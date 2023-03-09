a79b3e60b30b83034929af8c713f6459fe8ed2e3
SeedTechTrisha <balderas.trishaann@nexseed.net>
2023-03-09 10:56:41 +0800

    remove unnecessary files and change components name to camel case

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Login from './pages/login';
+import Login from './pages/Login';
 import { BrowserRouter } from 'react-router-dom';
 // import reportWebVitals from './reportWebVitals';
 
